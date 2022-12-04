3848225273891830a47336bc4d998fa6c986465e
ElviraMadamovich <110635987+ElviraMadamovich@users.noreply.github.com>
2022-12-04 22:39:17 +0300

    поправила классы

--- scripts/validate.js
+++ scripts/validate.js
@@ -9,12 +9,12 @@ const checkValidity = (myValidators, formElement, inputElement) => {
 
 const hideInputError = (errorElement, inputElement) => {
     errorElement.textContent = '';
-    inputElement.classList.remove('popup__content_type_error');
+    inputElement.classList.remove('popup__error_active');
 };
 
 const showInputError = (errorElement, inputElement) => {
     errorElement.textContent = inputElement.validationMessage;
-    inputElement.classList.add('popup__content_type_error');
+    inputElement.classList.add('popup__error_active');
 };
 
 const hasInvalidInput = (inputList) => {
@@ -69,6 +69,6 @@ enableValidation({
     inputSelector: ".popup__content",
     submitButtonSelector: ".popup__confirm",
     inactiveButtonClass: "popup__confirm_disabled",
-    inputErrorClass: "popup__content_type_error",
-    errorClass: "popup__content_type_error_active",
+    inputErrorClass: "popup__error",
+    errorClass: "popup__error_active",
 });
