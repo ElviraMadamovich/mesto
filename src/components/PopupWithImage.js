32424ff25cf704f4f1ce562fd249bd29a79a55c6
ElviraMadamovich <110635987+ElviraMadamovich@users.noreply.github.com>
2023-01-29 21:52:30 +0300

    отредактировала PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,12 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this._imageMax = this._popups.querySelector('.image-popup__pic');
-        this._imageName = this._popups.querySelector('.image-popup__name');
     }
 
     open(name, link) {
         super.open();   
+        this._imageMax = this._popups.querySelector('.image-popup__pic');
+        this._imageName = this._popups.querySelector('.image-popup__name');
         this._imageMax.src = link;
         this._imageName.textContent = name;
         this._imageMax.alt = name;
